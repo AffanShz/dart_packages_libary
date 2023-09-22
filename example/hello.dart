699903ac82f928cba7952dbf385a26a47ff5d146
affan <affanshahzada86@gmail.com>
2023-09-22 10:18:03 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_packages_library/helllo.dart';
+
+void main() {
+  print(sayHello("Aku"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
---
+++ lib/helllo.dart
@@ -0,0 +1,5 @@
+library dart_packages_library;
+
+export 'src/say_hello.dart';
+
+export 'src/customer.dart' show Category, Product, Customer;
